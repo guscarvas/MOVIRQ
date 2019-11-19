2320e31d24eeed01ba58fe10f814ccbc173dcde9
Unknown <guscarvas2010@gmail.com>
2019-11-19 17:53:07 -0300

    falta coisa ainda de user
    Parei em Login da Sessão no link : https://www.devmedia.com.br/ruby-on-rails-implementando-autenticacao-de-usuario/32124

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'users/new'
   get 'sobre_nos/index'
   get 'aaa/index'
+  resources :users
   resources :reservas
   resources :atleticas
+  
   root 'atleticas#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
