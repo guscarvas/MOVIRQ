7684aa628522a00582a02f3e3579378d127f0c83
luana-nunes <luana.dacostanunes@gmail.com>
2019-11-23 16:16:39 -0300

    acho q agr tao todos .scss check

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   resources :reservas
   resources :atleticas
   
-  root 'atleticas#index'
+  root 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
