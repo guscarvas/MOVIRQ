33ff94192a91603f3c745facf2223c567cc12599
luana-nunes <luana.dacostanunes@gmail.com>
2019-11-24 17:51:37 -0300

    front de reservas pontuais

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_11_24_184939) do
-=======
-ActiveRecord::Schema.define(version: 2019_11_24_155839) do
->>>>>>> master
 
   create_table "atleticas", force: :cascade do |t|
     t.string "name"
