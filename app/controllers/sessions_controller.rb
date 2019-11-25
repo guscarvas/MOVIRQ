3946aae9697db8ac95dc214ba8ba285cd56271a8
Eduardo Eiras de Carvalho <dueiras@gmail.com>
2019-11-25 14:21:21 -0300

    Auto stash before merge of "master" and "origin/master"
    "before action"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,15 @@ class SessionsController < ApplicationController
             if @user && @user.authenticate(params[:session][:password])
                         # sign_in(@user)
                         sign_in()
-            redirect_to @user
-    else
-            render 'new'
-    end
+                        redirect_to @user
+
+            elsif not @user #.authenticate(params[:session][:password])
+              redirect_to root_url
+            
+            else
+              render 'new'
+
+            end
   end
   def destroy
     sign_out
