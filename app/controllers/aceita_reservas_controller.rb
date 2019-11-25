a701b37a41635d72c0e8501ee65c46a64f1c7df3
luana-nunes <luana.dacostanunes@gmail.com>
2019-11-25 12:28:37 -0300

    chega

--- app/controllers/aceita_reservas_controller.rb
+++ app/controllers/aceita_reservas_controller.rb
@@ -11,7 +11,7 @@ class AceitaReservasController < ApplicationController
     #     end 
     #   end
     def show
-        @quadraHash = {101 => 'Quadra 1', 102 => 'Quadra 2', 103=> 'Quadra 3'}
+        @quadraHash = {101 => 'Quadra 1', 102 => 'Quadra 2', 103=> 'Quadra 3', 104 => 'Quadra 4', 105 => 'Quadra 5', 106 => 'Quadra 6', 107 => 'Quadra 7', 108 => 'Quadra 8', 109 => 'Quadra 9 A', 110 => 'Quadra 9 B', 111 => 'Quadra 10 A', 112 => 'Quadra 10 B', 113 => 'Velódromo A', 114 => 'Velódromo B', 115 => 'Módulo 1', 116 => 'Módulo 2', 117 => 'Módulo 3', 118 => 'Módulo 4'  }
         @diasHash = {1 => 'Segunda',2 => 'Terça',3 => 'Quarta', 4 => 'Quinta', 5 => 'Sexta'}
 
         
